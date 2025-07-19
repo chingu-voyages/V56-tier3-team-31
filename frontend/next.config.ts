9b41546b6ced9ba9f6cd2edb052d1f25d9ee1c17
Nguyen Vinh Quang <dragonlinuxnoinochi@gmail.com>
2025-07-19 21:57:46 +0700

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-      },
       // other Next.js configurations...
 };
 

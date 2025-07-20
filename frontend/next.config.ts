d187927b22ab54c2db0de023e595bb9c9abbede3
Thirsty Perry <vinhqua.io@gmail.com>
2025-07-20 08:19:10 +0700

    disable type error in run time build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-      // other Next.js configurations...
+  // other Next.js configurations...
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;

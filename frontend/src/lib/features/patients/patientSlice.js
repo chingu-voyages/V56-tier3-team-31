b4419efc60fab1648ffca9d1c151790d63ec1b9c
Thirsty Perry <vinhqua.io@gmail.com>
2025-07-28 16:27:03 +0700

    Remove Patient From Status Board

--- frontend/src/lib/features/patients/patientSlice.js
+++ frontend/src/lib/features/patients/patientSlice.js
@@ -19,19 +19,35 @@ export const patientSlice = createSlice({
   initialState,
   reducers: {
     updatePaitentStatusBoard: (state, { payload }) => {
-      const newDisplay = state.displayPatientStatus.map((patient) => {
-        if (patient.id === payload.id) {
-          return payload;
-        } else {
-          return patient;
-        }
-      });
+      let newDisplay = [];
+      const updatedPatient = state.displayPatientStatus.find(
+        (patient) => patient._id === payload._id
+      );
+      if (updatedPatient) {
+        newDisplay = state.displayPatientStatus.map((patient) => {
+          if (patient._id === payload._id) {
+            return payload;
+          } else {
+            return patient;
+          }
+        });
+      } else {
+        newDisplay = [...state.displayPatientStatus, payload];
+      }
+
       state.displayPatientStatus = newDisplay || [];
       toast("Patient Status Board Updated");
     },
     addPaitentToStatusBoard: (state, { payload }) => {
       const newDisplay = [...state.displayPatientStatus, payload];
 
+      state.displayPatientStatus = newDisplay || [];
+      toast("Patient Status Board Updated");
+    },
+    removePatientFromStatusBoard: (state, { payload }) => {
+      const newDisplay = state.displayPatientStatus.filter(
+        (patient) => patient._id !== payload._id
+      );
       state.displayPatientStatus = newDisplay || [];
       toast("Patient Status Board Updated");
     },
@@ -65,7 +81,10 @@ export const patientSlice = createSlice({
 });
 
 // Action creators are generated for each case reducer function
-export const { updatePaitentStatusBoard, addPaitentToStatusBoard } =
-  patientSlice.actions;
+export const {
+  updatePaitentStatusBoard,
+  addPaitentToStatusBoard,
+  removePatientFromStatusBoard,
+} = patientSlice.actions;
 
 export default patientSlice.reducer;
